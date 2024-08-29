802f75b668a27581e4c55fa2e9ff371fbce57388
pauluhlenkott <pauluhlenkott@gmail.com>
2024-08-28 18:31:44 -0700

    modify index html

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let puppyImages = [
+/*let puppyImages = [
   "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dpuppy&psig=AOvVaw2lhUrTaVQ8HV_xpItaPe77&ust=1724972147403000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNjHuZLkmIgDFQAAAAAdAAAAABAE",
   "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fpuppy&psig=AOvVaw2lhUrTaVQ8HV_xpItaPe77&ust=1724972147403000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNjHuZLkmIgDFQAAAAAdAAAAABAJ",
   "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fpuppy%2F&psig=AOvVaw2lhUrTaVQ8HV_xpItaPe77&ust=1724972147403000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNjHuZLkmIgDFQAAAAAdAAAAABAQ",
@@ -13,4 +13,25 @@ const imgs = document.getElementsByTagName("img");
 for (image of imgs) {
   const index = Math.floor(Math.random() * puppyImages.length);
   image.src = puppyImages[index];
-}
+}*/
+
+/*
+1. a start button and stop button
+2. Set duration
+3. Adding some music
+4. exhale enhale image
+5. how many times you meditated that day
+6. How many minutes total of meditation
+*/
+
+//Create a new html page
+//when extension is clicked, we will create new tap, whcih will be directed to url: index.html
+//create a start button and stop button
+/*const startButton = document.createElement("button");
+startButton.innerText = "Start";
+startButton.type = "button";
+//append those items to the document
+document.querySelector("body").appendChild(startButton);*/
+var div = document.createElement("div");
+document.body.appendChild(div);
+div.innerText = "test123";
